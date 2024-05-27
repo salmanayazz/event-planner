3d21782b49c44527ca7b8411a009858d13c1e0e1
Salman Ayaz <salmanayazz@gmail.com>
2024-05-26 22:55:02 -0700

    added endpoint to delete a group

--- server/src/main/java/com/example/server/controllers/GroupController.java
+++ server/src/main/java/com/example/server/controllers/GroupController.java
@@ -10,6 +10,7 @@ import com.example.server.security.jwt.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -56,6 +57,27 @@ public class GroupController {
         return ResponseEntity.ok().body("Group created successfully");
     }
 
+    @DeleteMapping("/{groupId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> deleteGroup(
+        @PathVariable("groupId") Long groupId,
+        HttpServletRequest req
+    ) {
+        Long userId = jwtUtils.getUserIdFromRequest(req);
+        Optional<Group> group = groupRepository.findById(groupId);
+
+        if (group.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Group not found");
+        }
+
+        if (!Objects.equals(group.get().getOwner().getId(), userId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Not authorized");
+        }
+
+        groupRepository.delete(group.get());
+        return ResponseEntity.ok().body("Successfully deleted Group");
+    }
+
     @PostMapping("/{groupId}/users")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> addUser(
@@ -87,9 +109,9 @@ public class GroupController {
     @DeleteMapping("/{groupId}/users/{userId}")
     @PreAuthorize("hasRole('USER')")
     ResponseEntity<?> removeUser(
-            @PathVariable("groupId") Long groupId,
-            @PathVariable("userId") Long userIdToRemove,
-            HttpServletRequest request
+        @PathVariable("groupId") Long groupId,
+        @PathVariable("userId") Long userIdToRemove,
+        HttpServletRequest request
     ) {
         Long userId = jwtUtils.getUserIdFromRequest(request);
         Group group = groupRepository.findOwned(userId, groupId);
